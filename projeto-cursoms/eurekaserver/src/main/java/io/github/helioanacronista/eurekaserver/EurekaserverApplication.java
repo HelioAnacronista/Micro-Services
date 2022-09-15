e8c3206cdc88af6120835ff53a21be4e87e1d32e
HelioAnacronista <heliojobs25@gmail.com>
2022-09-15 20:51:43 -0300

    1. Criação e Configuração do Discovery Server

---
+++ projeto-cursoms/eurekaserver/src/main/java/io/github/helioanacronista/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,16 @@
+package io.github.helioanacronista.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// @EnableEurekaServer para informar q vamos trabalhar com server. assim ele faz a configuraçao do eurika server
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
