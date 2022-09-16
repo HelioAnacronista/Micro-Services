10372a1d51aee516e16b0bff4572eff2f504e728
HelioAnacronista <heliojobs25@gmail.com>
2022-09-16 16:32:02 -0300

    Implementando método save clientes

--- projeto-cursoms/mscliente/src/main/java/io/github/helioanacronista/mscliente/application/ClientesController.java
+++ projeto-cursoms/mscliente/src/main/java/io/github/helioanacronista/mscliente/application/ClientesController.java
@@ -1,16 +1,44 @@
 package io.github.helioanacronista.mscliente.application;
 
+import io.github.helioanacronista.mscliente.application.representation.ClienteSaveRequest;
+import io.github.helioanacronista.mscliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesController {
+
+    private final ClienteService service;
 
     @GetMapping
     public String status() {
         return "ok";
     }
 
+    //Salvar cliente
+    public ResponseEntity save (@RequestBody ClienteSaveRequest request) {
+        Cliente cliente = request.toModel();
+        service.save(cliente);
+
+        /*
+        Criar uma Url corrente http//localhost:PORT/clientes?cpf=*********
+        Assim quando fazer get nessa url ele obter os dados do cliente
+         */
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+
+        return ResponseEntity.created(headerLocation).build();
+    }
 }
--- projeto-cursoms/mscliente/src/main/java/io/github/helioanacronista/mscliente/application/representation/ClienteSaveRequest.java
+++ projeto-cursoms/mscliente/src/main/java/io/github/helioanacronista/mscliente/application/representation/ClienteSaveRequest.java
@@ -1,2 +1,18 @@
-package io.github.helioanacronista.mscliente.application.representation;public class ClienteSaveRequest {
+package io.github.helioanacronista.mscliente.application.representation;
+
+import io.github.helioanacronista.mscliente.domain.Cliente;
+import lombok.Data;
+
+//Esse serio o DTO
+
+@Data
+public class ClienteSaveRequest {
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    //Vai conveter o json em obj de jpa para nosso entidade Cleinte
+    public Cliente toModel() {
+        return new Cliente(cpf,nome,idade);
+    }
 }
